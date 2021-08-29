b715378b09b4bfeed433fcd71e4822367774495a
Djotchuang <djotchuangtamo@gmail.com>
2021-08-29 03:10:45 +0100

    First commit with boiler plate

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div>
+           <h2>About Page</h2>
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime minus quas numquam impedit aut magnam laborum obcaecati officia ipsam laboriosam?</p> 
+        </div>
+    )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h2>The Home page</h2>
+    </div>
+  )
+}
+
