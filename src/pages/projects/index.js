1bde06ded06cdc0fe0175674fc92c9465e95d258
Djotchuang <djotchuangtamo@gmail.com>
2021-08-30 22:51:17 +0100

    Added portfolio items from graphql query (data is found in markdown files in projects folder)

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,43 @@
+import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/Layout"
 import * as styles from "../../styles/projects.module.css"
 
-export default function Projects() {
+export default function Projects({ data }) {
+  const projects = data.allMarkdownRemark.nodes
+
   return (
     <Layout>
       <div className={styles.portfolio}>
         <h2>Portfolio Projects</h2>
         <h3>Projects & Websites I've Worked On</h3>
+        <div className={styles.projects}>
+          {projects.map(project => (
+            <Link to={`/projects/${project.frontmatter.slug}`} key={project.id}>
+              <div>
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   )
 }
+
+// Export Projects Data Query
+export const query = graphql`
+  query ProjectsData {
+    allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        id
+        frontmatter {
+          slug
+          stack
+          title
+        }
+      }
+    }
+  }
+`
